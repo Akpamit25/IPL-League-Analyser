7aeb8b031ffd85ba3cafc2bb766ca46f1635e3d1
Amit Kumar <amitnitsri25@gmail.com>
2020-10-26 21:01:56 +0530

    UC3 : Batsamen with maximum no. of sixes and fours

--- src/main/java/com/capgemini/IPL/CricketAnalysis.java
+++ src/main/java/com/capgemini/IPL/CricketAnalysis.java
@@ -16,48 +16,67 @@ import builder.*;
 import exception.CSVBuilderException;
 
 public class CricketAnalysis {
-    List<IPLMostRuns> censusCSVList= null;
+	List<IPLMostRuns> censusCSVList = null;
 
-   public int loadData(String filePath) throws IOException {
-       try (BufferedReader reader = Files.newBufferedReader(Paths.get(filePath))) {
-           ICSVBuilder csvBuilder = CSVBuilderFactory.createCSVBuilder();
-           censusCSVList = csvBuilder.getCSVFileList(reader, IPLMostRuns.class);
-           System.out.println(censusCSVList);
-           return censusCSVList.size();
-       } catch (IOException e) {
-           throw new CSVBuilderException(e.getMessage(), CSVBuilderException.ExceptionType.FILE_NOT_FOUND);
-       } catch (RuntimeException e) {
-           throw new CSVBuilderException(e.getMessage(), CSVBuilderException.ExceptionType.INCORRECT_FILE);
-       }
-   }
-   public String getAverageWiseSorted() {
-       if (censusCSVList.size() == 0 || censusCSVList == null)
-           throw new CSVBuilderException("No Census Data", CSVBuilderException.ExceptionType.NO_CENSUS_DATA);
-       Comparator<IPLMostRuns> iplMostRunsComparator = Comparator.comparing(census -> census.Avg);
-       this.sort(iplMostRunsComparator);
-       String sortedCensusJson = new Gson().toJson(censusCSVList);
-       return sortedCensusJson;
-   }
-   
-   public String getStrikerateWiseSorted()  {
-       if(censusCSVList.size()==0 || censusCSVList==null)
-           throw new CSVBuilderException("No Census Data", CSVBuilderException.ExceptionType.NO_CENSUS_DATA);
-       Comparator<IPLMostRuns> iplMostRunsComparator = Comparator.comparing(census -> census.SR);
-       this.sort(iplMostRunsComparator);
-       String sortedCensusJson = new Gson().toJson(censusCSVList);
-       return sortedCensusJson;
-   }
+	public int loadData(String filePath) throws IOException {
+		try (BufferedReader reader = Files.newBufferedReader(Paths.get(filePath))) {
+			ICSVBuilder csvBuilder = CSVBuilderFactory.createCSVBuilder();
+			censusCSVList = csvBuilder.getCSVFileList(reader, IPLMostRuns.class);
+			System.out.println(censusCSVList);
+			return censusCSVList.size();
+		} catch (IOException e) {
+			throw new CSVBuilderException(e.getMessage(), CSVBuilderException.ExceptionType.FILE_NOT_FOUND);
+		} catch (RuntimeException e) {
+			throw new CSVBuilderException(e.getMessage(), CSVBuilderException.ExceptionType.INCORRECT_FILE);
+		}
+	}
 
-   private void sort(Comparator<IPLMostRuns> iplMostRunsComparator) {
-       for (int i = 0; i < censusCSVList.size() - 1; i++) {
-           for (int j = 0; j < censusCSVList.size() - i - 1; j++) {
-               IPLMostRuns census1 = censusCSVList.get(j);
-               IPLMostRuns census2 = censusCSVList.get(j + 1);
-               if (iplMostRunsComparator.compare(census1, census2) < 0) {
-                   censusCSVList.set(j, census2);
-                   censusCSVList.set(j + 1, census1);
-               }
-           }
-       }
-   }
+	public String getAverageWiseSorted() {
+		if (censusCSVList.size() == 0 || censusCSVList == null)
+			throw new CSVBuilderException("No Census Data", CSVBuilderException.ExceptionType.NO_CENSUS_DATA);
+		Comparator<IPLMostRuns> iplMostRunsComparator = Comparator.comparing(census -> census.Avg);
+		this.sort(iplMostRunsComparator);
+		String sortedCensusJson = new Gson().toJson(censusCSVList);
+		return sortedCensusJson;
+	}
+
+	public String getStrikerateWiseSorted() {
+		if (censusCSVList.size() == 0 || censusCSVList == null)
+			throw new CSVBuilderException("No Census Data", CSVBuilderException.ExceptionType.NO_CENSUS_DATA);
+		Comparator<IPLMostRuns> iplMostRunsComparator = Comparator.comparing(census -> census.SR);
+		this.sort(iplMostRunsComparator);
+		String sortedCensusJson = new Gson().toJson(censusCSVList);
+		return sortedCensusJson;
+	}
+
+	public String getFoursWiseSorted() {
+		if (censusCSVList.size() == 0 || censusCSVList == null)
+			throw new CSVBuilderException("No Census Data", CSVBuilderException.ExceptionType.NO_CENSUS_DATA);
+		Comparator<IPLMostRuns> iplMostRunsComparator = Comparator.comparing(census -> census.fours);
+		this.sort(iplMostRunsComparator);
+		String sortedCensusJson = new Gson().toJson(censusCSVList);
+		return sortedCensusJson;
+	}
+
+	public String getSixesWiseSorted() {
+		if (censusCSVList.size() == 0 || censusCSVList == null)
+			throw new CSVBuilderException("No Census Data", CSVBuilderException.ExceptionType.NO_CENSUS_DATA);
+		Comparator<IPLMostRuns> iplMostRunsComparator = Comparator.comparing(census -> census.sixs);
+		this.sort(iplMostRunsComparator);
+		String sortedCensusJson = new Gson().toJson(censusCSVList);
+		return sortedCensusJson;
+	}
+
+	private void sort(Comparator<IPLMostRuns> iplMostRunsComparator) {
+		for (int i = 0; i < censusCSVList.size() - 1; i++) {
+			for (int j = 0; j < censusCSVList.size() - i - 1; j++) {
+				IPLMostRuns census1 = censusCSVList.get(j);
+				IPLMostRuns census2 = censusCSVList.get(j + 1);
+				if (iplMostRunsComparator.compare(census1, census2) < 0) {
+					censusCSVList.set(j, census2);
+					censusCSVList.set(j + 1, census1);
+				}
+			}
+		}
+	}
 }
