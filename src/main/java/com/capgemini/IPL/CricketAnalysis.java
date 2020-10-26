82d4d6edf2425021a4d35fc772f9d3db9a649f18
Amit Kumar <amitnitsri25@gmail.com>
2020-10-26 18:58:57 +0530

    UC1 : Calculating Top Batting Averages

--- src/main/java/com/capgemini/IPL/CricketAnalysis.java
+++ src/main/java/com/capgemini/IPL/CricketAnalysis.java
@@ -9,21 +9,46 @@ import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Iterator;
+import java.util.*;
+import java.io.*;
+import com.google.gson.Gson;
+import builder.*;
+import exception.CSVBuilderException;
 
 public class CricketAnalysis {
-    public static int loadData(String filePath) throws IOException {
-        int totalRecords = 0;
-        try (Reader reader = Files.newBufferedReader(Paths.get(filePath))) {
-            CSVReader csvReader = new CSVReader(reader);
-            CsvToBean<IPLMostRuns> csvToBean = new CsvToBeanBuilder<IPLMostRuns>(csvReader).
-                    withType(IPLMostRuns.class)
-                    .build();
-            Iterator<IPLMostRuns> csvRecords = csvToBean.iterator();
-            while (csvRecords.hasNext()) {
-                totalRecords++;
-                csvRecords.next();
-            }
-        }
-        return totalRecords;
-    }
+    List<IPLMostRuns> censusCSVList= null;
+
+   public int loadData(String filePath) throws IOException {
+       try (BufferedReader reader = Files.newBufferedReader(Paths.get(filePath))) {
+           ICSVBuilder csvBuilder = CSVBuilderFactory.createCSVBuilder();
+           censusCSVList = csvBuilder.getCSVFileList(reader, IPLMostRuns.class);
+           System.out.println(censusCSVList);
+           return censusCSVList.size();
+       } catch (IOException e) {
+           throw new CSVBuilderException(e.getMessage(), CSVBuilderException.ExceptionType.FILE_NOT_FOUND);
+       } catch (RuntimeException e) {
+           throw new CSVBuilderException(e.getMessage(), CSVBuilderException.ExceptionType.INCORRECT_FILE);
+       }
+   }
+   public String getAverageWiseSorted() {
+       if (censusCSVList.size() == 0 || censusCSVList == null)
+           throw new CSVBuilderException("No Census Data", CSVBuilderException.ExceptionType.NO_CENSUS_DATA);
+       Comparator<IPLMostRuns> iplMostRunsComparator = Comparator.comparing(census -> census.Avg);
+       this.sort(iplMostRunsComparator);
+       String sortedCensusJson = new Gson().toJson(censusCSVList);
+       return sortedCensusJson;
+   }
+
+   private void sort(Comparator<IPLMostRuns> iplMostRunsComparator) {
+       for (int i = 0; i < censusCSVList.size() - 1; i++) {
+           for (int j = 0; j < censusCSVList.size() - i - 1; j++) {
+               IPLMostRuns census1 = censusCSVList.get(j);
+               IPLMostRuns census2 = censusCSVList.get(j + 1);
+               if (iplMostRunsComparator.compare(census1, census2) < 0) {
+                   censusCSVList.set(j, census2);
+                   censusCSVList.set(j + 1, census1);
+               }
+           }
+       }
+   }
 }
--- src/main/java/model/IPLMostRuns.java
+++ src/main/java/model/IPLMostRuns.java
@@ -4,133 +4,176 @@ import com.opencsv.bean.CsvBindByName;
 
 public class IPLMostRuns {
     @CsvBindByName(column = "POS")
-    private String POS;
+    public String POS;
 
     @CsvBindByName(column = "PLAYER")
-    private String PLAYER;
+    public String PLAYER;
 
     @CsvBindByName(column = "Mat")
-    private String Mat;
+    public String Mat;
 
     @CsvBindByName(column = "Inns")
-    private String Inns;
+    public String Inns;
 
     @CsvBindByName(column = "NO")
-    private String NO;
+    public String NO;
 
     @CsvBindByName(column = "Runs")
-    private String Runs;
+    public String Runs;
 
     @CsvBindByName(column = "HS")
-    private String HS;
+    public String HS;
 
     @CsvBindByName(column = "Avg")
-    private String Avg;
+    public double Avg;
 
     @CsvBindByName(column = "BF")
-    private String BF;
+    public String BF;
 
     @CsvBindByName(column = "SR")
-    private String SR;
+    public double SR;
 
     @CsvBindByName(column = "100")
-    private String hundred;
+    public String hundred;
 
     @CsvBindByName(column = "50")
-    private String fifty;
+    public String fifty;
 
     @CsvBindByName(column = "4s")
-    private String fours;
+    public String fours;
 
     @CsvBindByName(column = "6s")
-    private String sixs;
+    public String sixs;
 
     public String getPOS() {
         return POS;
     }
-    public void setPOS(String POS)
-    {
+
+    public void setPOS(String POS) {
         this.POS = POS;
     }
-    public String getPLAYER()
-    {
+
+    public String getPLAYER() {
         return PLAYER;
     }
-    public void setPLAYER(String PLAYER)
-    {
+
+    public void setPLAYER(String PLAYER) {
         this.PLAYER = PLAYER;
     }
-    public String getMat()
-    {
+
+    public String getMat() {
         return Mat;
     }
+
     public void setMat(String mat) {
         Mat = mat;
     }
+
     public String getInns() {
         return Inns;
     }
+
     public void setInns(String inns) {
         Inns = inns;
     }
+
     public String getNO() {
         return NO;
     }
+
     public void setNO(String NO) {
         this.NO = NO;
     }
+
     public String getRuns() {
         return Runs;
     }
+
     public void setRuns(String runs) {
         Runs = runs;
     }
+
     public String getHS() {
         return HS;
     }
+
     public void setHS(String HS) {
         this.HS = HS;
     }
-    public String getAvg() {
+
+    public double getAvg() {
         return Avg;
     }
-    public void setAvg(String avg) {
+
+    public void setAvg(double avg) {
         Avg = avg;
     }
+
     public String getBF() {
         return BF;
     }
+
     public void setBF(String BF) {
         this.BF = BF;
     }
-    public String getSR() {
+
+    public double getSR() {
         return SR;
     }
-    public void setSR(String SR) {
+
+    public void setSR(double SR) {
         this.SR = SR;
     }
+
     public String getHundred() {
         return hundred;
     }
+
     public void setHundred(String hundred) {
         this.hundred = hundred;
     }
+
     public String getFifty() {
         return fifty;
     }
+
     public void setFifty(String fifty) {
         this.fifty = fifty;
     }
+
     public String getFours() {
         return fours;
     }
+
     public void setFours(String fours) {
         this.fours = fours;
     }
+
     public String getSixs() {
         return sixs;
     }
+
     public void setSixs(String sixs) {
         this.sixs = sixs;
+    }
+
+    @Override
+    public String toString() {
+        return "IplMostRuns{" +
+                "POS='" + POS + '\'' +
+                ", PLAYER='" + PLAYER + '\'' +
+                ", Mat='" + Mat + '\'' +
+                ", Inns='" + Inns + '\'' +
+                ", NO='" + NO + '\'' +
+                ", Runs='" + Runs + '\'' +
+                ", HS='" + HS + '\'' +
+                ", Avg='" + Avg + '\'' +
+                ", BF='" + BF + '\'' +
+                ", SR='" + SR + '\'' +
+                ", hundred='" + hundred + '\'' +
+                ", fifty='" + fifty + '\'' +
+                ", fours='" + fours + '\'' +
+                ", sixs='" + sixs + '\'' +
+                '}';
     }
 }
